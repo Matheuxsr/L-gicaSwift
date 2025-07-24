8788bffa936aa6989c18f6f0ae233e12a4788496
Matheus Araújo <matheusracoelho1@gmail.com>
2025-07-24 15:08:50 -0300

    Add files via upload

---
+++ main4.swift
@@ -0,0 +1,15 @@
+
+
+import Foundation
+
+let farol = "vermelho"
+
+if farol == "amarelo" {
+    print("Preste atenção!")
+} else if farol == "verde" {
+    print("Prossiga!")
+} else if farol == "vermelho" {
+    print("Fechado")
+} else {
+    print("Cor inválida")
+}
