ad54b12b9ccd87d6a53a379e6be6ef679b22f9c0
Matheus Araújo <matheusracoelho1@gmail.com>
2025-07-24 15:13:26 -0300

    Add files via upload

---
+++ main0.swift
@@ -0,0 +1,17 @@
+/**
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*/
+ 
+ let idade = 25
+ let possuiCarteira = true 
+ 
+ if idade >= 18 && possuiCarteira {
+     print("Pode dirigir.")
+ } else {
+     print("Não pode dirigir.")
+ }
